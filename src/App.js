68dc0e976a7e4a39cb658656a6d9916ce8ce2607
cpeppsi <caepps28@gmail.com>
2023-06-06 19:33:39 -0400

    Adding artist and album views

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
 import { useState, useEffect } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import { DataContext } from './Contexts/DataContext'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './Components/Gallery'
+import SearchBar from './Components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
-function App(){
-  const [search, setSearch] = useState('')
-  const [message, setMessage] = useState('Search for Music!')
-  const [data, setData] = useState([])
+function App() {
+    const [search, setSearch] = useState('')
+    const [message, setMessage] = useState('Search for Music!')
+    const [data, setData] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const API_URL = `https://itunes.apple.com/search?term=${encodeURI(search)}`
-      console.log(API_URL)
-      const response = await fetch(API_URL)
-      const data = await response.json()
-      console.log(data)
-      if (data.results.length > 0) {
-        setData(data.results)
-      } else {
-        setMessage('Not found')
+    useEffect(() => {
+      const fetchData = async () => {
+        const API_URL = `https://itunes.apple.com/search?term=${encodeURI(search)}`
+        console.log(API_URL)
+        const response = await fetch(API_URL)
+        const data = await response.json()
+        console.log(data)
+        if (data.results.length > 0) {
+          setData(data.results)
+        } else {
+          setMessage('Not found')
+        }
       }
+
+      if (search) fetchData()
+    }, [search])
+
+    const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
     }
 
-    if (search) fetchData()
-  }, [search])
-
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-  }
-
-  return (
-    <div>
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
-    </div>
-  )
+    return (
+        <div>
+            {message}
+            <Router>
+              <Routes>
+                <Route path='/' element={
+                  <>
+                    <SearchBar handleSearch={handleSearch} />
+                    <Gallery data={data} />
+                  </>
+                } />
+                <Route path='/album/:id' element={<AlbumView />} />
+                <Route path='/artist/:id' element={<ArtistView />} />
+              </Routes>
+            </Router>
+        </div>
+    )
 }
-  
+
 export default App
---
+++ src/components/ArtistView.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function ArtistView() {
+    const { id } = useParams()
+    const [artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/album/${id}`
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            const albums = data.results.filter(item => item.collectionType === 'Album')
+            console.log(albums)
+            setArtistData(albums)
+        }
+
+        fetchData()
+    }, [id])
+
+    const navigate = useNavigate()
+
+    const navButtons = () => {
+      
+    }
+
+    const display = artistData && artistData.map(album => {
+        return (
+            <div key={album.collectionId}>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+          {navButtons()}
+          {display}
+        </div>
+    )
+}
+
+export default ArtistView
