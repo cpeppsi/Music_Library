bed4e8164f4197584b8feae051ca9573f9526284
cpeppsi <caepps28@gmail.com>
2023-06-07 14:54:13 -0400

    Updating App.js and Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import Gallery from './components/Gallery'
-import SearchBar from './components/Searchbar'
+import SearchBar from './components/SearchBar'
 
 function App(){
   const [search, setSearch] = useState('')
